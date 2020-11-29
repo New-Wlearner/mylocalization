f376264b490639e313561180bbd4e75402fba0fc
dongxiao <you@example.com>
2020-11-29 16:15:56 +0800

    sensor_data receive

---
+++ src/mylocalization/include/mylocalization/sensor_data/gnss_data.h
@@ -0,0 +1,37 @@
+#ifndef MYLOCALIZATION_SENSOR_DATA_GNSS_DATA_H_
+#define MYLOCALIZATION_SENSOR_DATA_GNSS_DATA_H_
+
+#include <deque>
+
+#include "Geocentric/LocalCartesian.hpp"
+
+namespace mylocalization
+{
+    class GNSSData
+    {
+    private:
+        // ?
+        static GeographicLib::LocalCartesian geo_converter;
+        static bool origin_position_inited;
+    public:
+      double time = 0.0;
+      double longtitude = 0.0;
+      double latitude = 0.0;
+      double altitude = 0.0;
+      double local_E = 0.0;
+      double local_N = 0.0;
+      double local_U = 0.0;
+      // 这两个是干啥的
+      int status = 0;
+      int service = 0;  
+
+      void InitOriginPosition();
+      void UpdateXYZ();
+      static bool SyncData(std::deque<GNSSData>& UnsyncedData,std::deque<GNSSData> SyncedData, double sync_time);
+    };
+    
+}
+
+
+
+#endif
